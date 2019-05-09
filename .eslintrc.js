9a162a5b58931ccea530ce3393d82705352d0f8d
Chris Capper <chris.capperea@gmail.com>
2019-05-09 21:45:08 +0100

    assertion plugins added

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,14 @@ module.exports = {
     "parserOptions": {
         "ecmaVersion": 2018
     },
+    "plugins": [
+        "chai-friendly",
+        "chai-expect"
+    ],
     "rules": {
+        "no-unused-expressions": 0,
+        "chai-friendly/no-unused-expressions": 2,
+        "chai-expect/missing-assertion": 2,
+        "chai-expect/terminating-properties": 1
     }
 };
